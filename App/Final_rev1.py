42a6cc95a3b857863c218bb1ba5c240236744dab
Ed Lee <76930820+rageofpaju@users.noreply.github.com>
2023-01-25 01:26:30 +0900

    Update Final_rev1.py

--- App/Final_rev1.py
+++ App/Final_rev1.py
@@ -211,7 +211,7 @@ else:
     st.text("")
     st.write(
     '''
-    # :cook: Here's one recipe for your need :cook:
+    # :cook: Here's a for recipe for you :cook:
     ''')
     #food_suggestion = filter_calories[['Name', 'RecipeInstructions','RecipeIngredientParts','Calories']].sample(1)
     food_suggestion = filter_calories.sample(1)
