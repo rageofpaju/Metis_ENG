54ec95df4046c0169960fff41f325aaed8cc78e4
Ed Lee <76930820+rageofpaju@users.noreply.github.com>
2023-01-24 15:36:57 +0900

    Update MVP_rev1.py

--- App/MVP_rev1.py
+++ App/MVP_rev1.py
@@ -88,13 +88,14 @@ st.write(
 #df = pd.read_csv('df_recipe_topic_labeled.csv')
 #df = pd.read_csv('gs://bucket_lhs/eng_db/df_recipe_topic_labeled.csv')
 #df = pd.read_csv('/app/metis_eng/App/df_recipe_topic_labeled_mvp_reduced.csv')
+df = pd.read_csv('https://s3.us-west-004.backblazeb2.com/metis-eng-edlee/df_recipe_topic_labeled_mvp_reduced.csv')
 
 #engine_recipes = create_engine('sqlite:///mvp_recipe_reduced.db').connect()
 #st.write('test')
 
-con = sqlite3.connect('https://s3.us-west-004.backblazeb2.com/metis-eng-edlee/mvp_recipe_reduced.db')
-cur = con.cursor()
-df = pd.read_sql_query('SELECT * FROM df_recipe_topic_labeled_mvp_reduced', con)
+#con = sqlite3.connect('https://s3.us-west-004.backblazeb2.com/metis-eng-edlee/mvp_recipe_reduced.db')
+#cur = con.cursor()
+#df = pd.read_sql_query('SELECT * FROM df_recipe_topic_labeled_mvp_reduced', con)
 
 #con = sqlite3.connect('/app/metis_eng/App/mvp_recipe_reduced.db')
 #cur = con.cursor()
