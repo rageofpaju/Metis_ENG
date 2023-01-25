7d063114fc90bfc2102bef2ba0065307fb76a6f6
Ed Lee <76930820+rageofpaju@users.noreply.github.com>
2023-01-25 11:25:23 +0900

    Update fridge_scrounger_project_rev1.py

--- App/fridge_scrounger_project_rev1.py
+++ App/fridge_scrounger_project_rev1.py
@@ -75,11 +75,11 @@ with col2:
 # For testing
 #df = pd.read_csv('C:/Git Storage/Engineering/df_recipe_topic_labeled_eng_reduced.csv')
 
-# For real thing (github)
-#df = pd.read_csv('/app/metis_eng/App/df_recipe_topic_labeled_eng_reduced.csv')
+# For showcase (github)
+df = pd.read_csv('/app/metis_eng/App/df_recipe_topic_labeled_eng_reduced.csv')
 
 # For real thing (backblaze)
-df = pd.read_csv('https://s3.us-west-004.backblazeb2.com/metis-eng-edlee/df_recipe_topic_labeled_mvp_reduced.csv')
+#df = pd.read_csv('https://s3.us-west-004.backblazeb2.com/metis-eng-edlee/df_recipe_topic_labeled_mvp_reduced.csv')
 #df = pd.read_csv(data_url)
 
 
